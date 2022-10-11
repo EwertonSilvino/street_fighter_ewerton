ac4537f03bdebabbd5f86695f1ddecf46626a66f
Your Name <you@example.com>
2022-10-11 16:27:22 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+
+function img01() {
+    document.getElementById("random").src= 'img/akuma.png';
+} 
+
+function img02() {
+    document.getElementById("random").src= 'img/ken3.png';
+} 
+    
+function img03() {
+    document.getElementById("random").src= 'img/ryu.png';
+} 
+    
+function img04() {
+    document.getElementById("random").src= 'img/chum-li.png';
+} 
+    
+
+function img0() {
+    document.getElementById("random").src= 'img/int.png';
+} 
+    
+
+    
+    
+   
+    
+  
+
+    
